9add63a5c934caad5b4574ac2f4f29705738914b
Jeferson Gomes <67127121+jeferson3@users.noreply.github.com>
2022-07-21 00:56:21 -0300

    att routes

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,8 @@ Route::group(['prefix' => '/auth'], function (){
 
 Route::group(['prefix' => '/', 'middleware' => 'jwt'], function (){
     Route::get('/posts', [\App\Http\Controllers\Customer\CustomerController::class, 'index']);
-    Route::post('/posts/{post}/comment', [\App\Http\Controllers\Customer\CustomerController::class, 'comment']);
-    Route::post('/posts/{post}/like', [\App\Http\Controllers\Customer\CustomerController::class, 'like']);
+    Route::post('/posts/{post}/comments', [\App\Http\Controllers\Customer\CustomerController::class, 'comment']);
+    Route::post('/posts/{post}/likes', [\App\Http\Controllers\Customer\CustomerController::class, 'like']);
     Route::group(['prefix' => '/admin'], function (){
         Route::apiResource('posts', \App\Http\Controllers\Admin\PostController::class);
     });
